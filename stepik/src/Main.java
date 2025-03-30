438d44f5d5970c6ebae2f97f22c5a1f7a6f8d72f
Benchik31 <Bendzhaminy@mail.ru>
2025-03-30 13:47:56 +0300

    add figure

--- stepik/src/Main.java
+++ stepik/src/Main.java
@@ -3,3 +3,71 @@ public class Main {
         System.out.println("Hello World");
     }
 }
+abstract class Shape {
+    abstract double area();
+    abstract double perimeter();
+}
+
+class Triangle extends Shape {
+    @Override
+    double area() { return 0; }
+    @Override
+    double perimeter() { return 0; }
+}
+
+class IsoscelesTriangle extends Triangle {}
+class EquilateralTriangle extends Triangle {}
+class RightTriangle extends Triangle {}
+
+class Quadrilateral extends Shape {
+    @Override
+    double area() { return 0; }
+    @Override
+    double perimeter() { return 0; }
+}
+
+class ConvexQuadrilateral extends Quadrilateral {}
+class Parallelogram extends ConvexQuadrilateral {}
+class Rectangle extends Parallelogram {}
+class Square extends Rectangle {}
+class Rhombus extends Parallelogram {}
+class Trapezoid extends ConvexQuadrilateral {}
+
+class Pentagon extends Shape {
+    @Override
+    double area() { return 0; }
+    @Override
+    double perimeter() { return 0; }
+}
+
+class Hexagon extends Shape {
+    @Override
+    double area() { return 0; }
+    @Override
+    double perimeter() { return 0; }
+}
+
+class Oval extends Shape {
+    @Override
+    double area() { return 0; }
+    @Override
+    double perimeter() { return 0; }
+}
+
+class Circle extends Oval {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
