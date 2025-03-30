a7632e7827b01abe8d17b477d5a8688fd44b8e41
Benchik31 <Bendzhaminy@mail.ru>
2025-03-30 22:12:05 +0300

    реализовал прорисовку снеговика в swing

--- stepik/src/swing1/Snowman.java
+++ stepik/src/swing1/Snowman.java
@@ -9,42 +9,34 @@ public class Snowman extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // Фон
         setBackground(Color.CYAN);
 
         Graphics2D g2 = (Graphics2D) g;
 
-        // Нижний шар
         g2.setColor(Color.WHITE);
         g2.fillOval(100, 200, 200, 200);
 
-        // Средний шар
         g2.fillOval(120, 120, 160, 160);
 
-        // Голова
         g2.fillOval(140, 60, 120, 120);
 
-        // Глаза
         g2.setColor(Color.BLACK);
         g2.fillOval(170, 100, 10, 10);
         g2.fillOval(220, 100, 10, 10);
 
-        // Морковка (нос)
         g2.setColor(Color.ORANGE);
         int[] x = {195, 250, 195};
         int[] y = {130, 135, 140};
         g2.fillPolygon(x, y, 3);
 
-        // Руки
-        g2.setColor(new Color(139, 69, 19)); // коричневый
+        g2.setColor(new Color(139, 69, 19));
         g2.setStroke(new BasicStroke(5));
-        g2.drawLine(120, 160, 50, 100); // левая
-        g2.drawLine(280, 160, 350, 100); // правая
+        g2.drawLine(120, 160, 50, 100);
+        g2.drawLine(280, 160, 350, 100);
 
-        // Шляпа
         g2.setColor(Color.BLACK);
-        g2.fillRect(160, 40, 80, 20); // верхняя часть
-        g2.fillRect(145, 60, 110, 10); // поля
+        g2.fillRect(160, 40, 80, 20);
+        g2.fillRect(145, 60, 110, 10);
     }
 
     public static void main(String[] args) {
